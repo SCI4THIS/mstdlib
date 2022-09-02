1126b2b534e85f8200d9ff41a180245c6a787cf9
Andrew Kistler <akistler@monetra.com>
2022-09-02 09:12:46 -0400

    Cleanup. Got test 6 working.
    modified:   formats/CMakeLists.txt
    renamed:    formats/http2/m_http2_cipher.c -> formats/http2/m_http2.c
    deleted:    formats/http2/m_http2_frame.c
    deleted:    formats/http2/m_http2_huffman.c
    modified:   test/formats/check_http2_reader.c

--- formats/http2/m_http2_huffman.c
+++
@@ -1,6 +0,0 @@
-#include <mstdlib/mstdlib.h>
-#include <mstdlib/formats/m_http2.h>
-
-#include "generated/m_http2_huffman_generated_encode.c"
-#include "generated/m_http2_huffman_generated_decode.c"
-
